66d9cf5001c6c219aa5133e3085016c6f1590b7b
mikel6001 <michaeljohn.roguel@awsys-i.com>
2024-10-01 13:55:43 +0800

    Base template intro

--- mysite_tut/main/views.py
+++ mysite_tut/main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>tech with me!<h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/base.html", {})
+    # ls = ToDoList.objects.get(name=name)
+    # item = ls.item_set.get(id=1)
+    # return HttpResponse("<h1>%s<h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
 
-def v1(response):
-    return HttpResponse("<h1>view1!<h1>")
+def home(response):
+    return render(response, "main/home.html", {})
